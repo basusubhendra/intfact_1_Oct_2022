cd2f56cafa8add671e52f020ef635654da1ad3b6
Subhendra Basu <basu.subhendra@outlook.com>
2022-10-01 09:47:40 +0000

    First Commit

---
+++ intfact.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    char* num = strdup(argv[1]);
+    return 0;
+}
